1f49f6b4dd292447f18a7530d25f98278c2495f0
Alessander Leon <alessanderleon@gmail.com>
2025-03-11 00:15:18 +0000

    Generador de cartas aleatorio listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
+  //["♦", "♥", "♠", "♣"] ["Diamante", "Corazon", "Pica", "Trebol"]
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"]
+  let numerosCarta = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+  let palosRandom = Math.floor(Math.random() * palos.length)
+  let numerosRandom = Math.floor(Math.random() * numerosCarta.length)
+    
+  let valorRandom = document.getElementById("numeroCarta")
+  valorRandom.innerHTML = numerosCarta[numerosRandom]
+
+  let naipeOne = document.getElementById("naipe-one")
+  naipeOne.innerHTML = palos[palosRandom]
+
+  let naipeTwo = document.getElementById("naipe-two")
+  naipeTwo.innerHTML = palos[palosRandom]
+
+
+  function colorNaipe() {
+
+    if(palos[palosRandom] === "♦" || palos[palosRandom] === "♥") {
+      document.getElementById("naipe-one").style.color = "red";
+      document.getElementById("naipe-two").style.color = "red";
+    } else {
+      document.getElementById("naipe-one").style.color = "black";
+      document.getElementById("naipe-two").style.color = "black"
+    }
+}
+colorNaipe()
 };
